89d0303a10dd5782d933a7efb108317a881fb217
Ozias Conrado Empreendedor <placasteell@gmail.com>
2025-06-22 17:55:15 +0000

    Não corrigiu o erro do login!

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -108,15 +108,6 @@ export default function RegisterPage() {
               Cadastrar
             </Button>
           </form>
-          <div className="relative my-4">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t" />
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">Ou cadastre-se com</span>
-            </div>
-          </div>
-          <GoogleSignInButton actionText="Cadastrar com Google" />
         </CardContent>
         <CardFooter className="flex justify-center">
           <p className="text-sm">
