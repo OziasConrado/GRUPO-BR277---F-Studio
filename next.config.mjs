85e2c0d6178c54f43f67cae59f16f959f0c7d64c
Ozias Conrado Empreendedor <placasteell@gmail.com>
2025-06-25 00:27:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    // This is required to allow the Next.js dev server to accept requests from the preview server.
-    allowedDevOrigins: [
-      'https://*.cluster-etsqrqvqyvd4erxx7qq32imrjk.cloudworkstations.dev',
-    ],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
       },
     ],
   },
