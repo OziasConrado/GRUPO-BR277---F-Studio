b1f55bd15268a00939868952e011c33412ff71b4
Ozias Conrado Empreendedor <placasteell@gmail.com>
2025-06-26 20:25:23 +0000

    Falhou de novo.

--- next.config.ts
+++
@@ -1,1 +0,0 @@
-// This file is deprecated. Use next.config.mjs instead.
--- src/next.config.ts
+++
@@ -1,3 +0,0 @@
-
-// Este arquivo foi intencionalmente deixado em branco para evitar conflitos com o next.config.ts na raiz.
-
