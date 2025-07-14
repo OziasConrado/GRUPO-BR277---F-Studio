9f1952ad621064d380201689101c435faab0685c
Ozias Conrado Empreendedor <placasteell@gmail.com>
2025-07-14 22:41:46 +0000

    A imagem não carregou: https://firebasestorage.googleapis.com/v0/b/grupo

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,22 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+                port: '',
+                pathname: '/v0/b/grupo-br277.appspot.com/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
