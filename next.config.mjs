6364a39746db29cec2aab2092e4228f3d437428d
Ozias Conrado Empreendedor <placasteell@gmail.com>
2025-07-28 21:26:33 +0000

    Tá acusando esses erros e o app não está carregando:

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,8 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-                port: '',
-                pathname: '/v0/b/grupo-br277.appspot.com/**',
-            },
-        ],
-    },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
