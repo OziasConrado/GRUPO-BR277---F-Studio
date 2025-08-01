eaec26e78f815e357e953e92e5d27b8122c727c5
Ozias Conrado Empreendedor <placasteell@gmail.com>
2025-08-01 02:20:49 +0000

    A tela continua toda branca

--- workspace/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type {Config} from 'next';
-
-const nextConfig: Config = {
-  // A configuração de imagens foi removida para usar o padrão do Next.js
-  // e corrigir o erro de build.
-};
-
-export default nextConfig;
