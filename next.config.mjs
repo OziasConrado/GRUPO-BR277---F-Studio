6121d2a62a2f0cc5131ebb025be6ea6d1fcc3e1a
Ozias Conrado Empreendedor <placasteell@gmail.com>
2025-10-27 13:47:01 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,39 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
         port: '',
-        pathname: '**',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloud.fullcam.me',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'playerv.logicahost.com.br',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'www.giseleimoveis.com.br',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
