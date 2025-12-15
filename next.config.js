d1e713400d398ad962a8ec3762cb05b8647d0138
Ozias Conrado Empreendedor <placasteell@gmail.com>
2025-12-15 00:33:58 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://firebas

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
   async redirects() {
     return [
       {
