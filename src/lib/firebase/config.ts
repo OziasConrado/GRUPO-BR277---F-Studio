84b486b8c0734deccce10b7ea489a6df4d351ccd
Ozias Conrado Empreendedor <placasteell@gmail.com>
2025-12-26 18:56:17 +0000

    Try fixing this error: `Console Error: Firebase Auth Error: "auth/api-ke

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,13 +1,12 @@
 // NOTE: This is for client-side code.
 // Don't put anything sensitive in here.
-// This is a workaround for environment variables not being populated correctly in the client.
 
 export const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  authDomain: "grupobr277-v2-d85f5.firebaseapp.com",
+  projectId: "grupobr277-v2-d85f5",
+  storageBucket: "grupobr277-v2-d85f5.appspot.com",
+  messagingSenderId: "338275344335",
+  appId: "1:338275344335:web:91a0c4f8d95e8657631168",
+  measurementId: "G-L5G7P4M65P"
 };
