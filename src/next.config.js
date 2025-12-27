4374503066d895824d90ac62f1a283ee955e4a4e
Ozias Conrado Empreendedor <placasteell@gmail.com>
2025-12-27 22:31:21 +0000

    Sucesso!

---
+++ src/next.config.js
@@ -0,0 +1,31 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/streaming',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
